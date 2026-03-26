a08ec39feb3b70719a631787fe55c843bb65041f
Michael Weinberg <michaelweinberg@Michaels-MacBook-Pro.local>
2026-03-26 09:52:12 -0700

    feat: initial commit

---
+++ src/main/kotlin/com/salon/inventoryservice/InventoryserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.salon.inventoryservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InventoryserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<InventoryserviceApplication>(*args)
+}
