42cf687ca1f0dd1fbe9f2ba69023623ece6d787d
Michael Weinberg <michaelweinberg@Michaels-MacBook-Pro.local>
2026-04-01 17:09:45 -0700

    feat: add mongo support

--- src/main/kotlin/com/salon/inventoryservice/domain/InventoryItem.kt
+++ src/main/kotlin/com/salon/inventoryservice/domain/InventoryItem.kt
@@ -1,6 +1,11 @@
 package com.salon.inventoryservice.domain
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "inventory_items")
 data class InventoryItem(
+    @Id
     val id: String,
     val name: String,
     val category: String,
